9c2da052654dc86fd9e7d794084f1f000af8ac48
omar nabil <omarnabilali4@gmail.com>
2026-05-04 15:44:04 +0300

    appBar done

--- lib/features/home/presentation/pages/widgets/custom_app_bar.dart
+++ lib/features/home/presentation/pages/widgets/custom_app_bar.dart
@@ -9,7 +9,23 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
     return AppBar(
       backgroundColor: Colors.white,
       elevation: 0,
-      title: CustomTextField(hintText: 'snndn'),
+      title: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 15.0),
+        child: TextField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+              borderSide: BorderSide(color: Colors.grey),
+            ),
+
+            hintText: 'Search...',
+          ),
+          onChanged: (value) {
+            // هنا تعمل الفلترة
+            print(value);
+          },
+        ),
+      ),
 
       actions: [
         IconButton(
